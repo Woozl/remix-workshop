f12ac85f7df7a18ddb611bc725ae59ef4714038f
David Glymph <Woozl@users.noreply.github.com>
2022-10-31 11:47:37 -0400

    02 data loading

--- exercise/02-data-loading/app/routes/posts/index.tsx
+++ exercise/02-data-loading/app/routes/posts/index.tsx
@@ -1,7 +1,38 @@
+import { Link, useLoaderData } from "@remix-run/react";
+import { json, LoaderArgs } from "@remix-run/server-runtime";
+import { prisma } from "~/db.server";
+
+export const loader = async({request}: LoaderArgs) => {
+  const data = {
+    posts: [
+      {
+        slug: "my-first-post",
+        title: "My First Post",
+      },
+      {
+        slug: "90s-mixtape",
+        title: "A Mixtape I Made Just For You",
+      },
+    ],
+  };
+  // const posts = await prisma.posts.findMany();
+  return json(data);
+}
+
 export default function Posts() {
+  const { posts } = useLoaderData<typeof loader>();
   return (
     <main>
       <h1>Posts</h1>
+      <ul>
+        {posts.map((post: any) => (
+          <li key={post.slug}>
+            <Link to={post.slug} className="text-blue-600 underline">
+              {post.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
