b6bf2a6ca4fcf1cb69ad0099e006afcee0cb7823
David Glymph <Woozl@users.noreply.github.com>
2022-10-31 14:55:45 -0400

    06 progressive enchancement + prefetching

--- exercise/06-progressive-enhancement/app/routes/posts/admin/new.tsx
+++ exercise/06-progressive-enhancement/app/routes/posts/admin/new.tsx
@@ -1,6 +1,6 @@
 import type { ActionArgs } from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
-import { Form, useActionData } from "@remix-run/react";
+import { Form, useActionData, useTransition } from "@remix-run/react";
 import invariant from "tiny-invariant";
 
 import { createPost } from "~/models/post.server";
@@ -39,6 +39,9 @@ export default function NewPost() {
   // 🐨 get the transition from useTransition
   // 🦉 make sure that useTransition comes from "@remix-run/react" and NOT "react"!!
   // 🐨 determine whether we're creating by whether there's a transition.submission object.
+
+  const transistion = useTransition();
+  const isCreatingPost = Boolean(transistion.submission);
 
   return (
     <Form method="post">
@@ -80,9 +83,10 @@ export default function NewPost() {
           type="submit"
           className="rounded bg-blue-500 py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400 disabled:bg-blue-300"
           // 🐨 add a disabled attribute to the button if we're creating
+          disabled={isCreatingPost}
         >
           {/* 🐨 Have this say "Creating..." if we're in a creating state */}
-          Create Post
+          {!isCreatingPost ? "Create Post" : "Creating..."}
         </button>
       </p>
     </Form>
