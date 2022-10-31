c837a636e8a4ecd08e4f6e5f5e3088d0c0ba8c1d
David Glymph <Woozl@users.noreply.github.com>
2022-10-31 15:56:28 -0400

    07 multiple forms

--- exercise/07-multiple-forms/app/routes/posts/admin/$slug.tsx
+++ exercise/07-multiple-forms/app/routes/posts/admin/$slug.tsx
@@ -9,7 +9,7 @@ import {
 import invariant from "tiny-invariant";
 
 // 🐨 you'll need to import `deletePost` and `updatePost` here as well.
-import { createPost, getPost } from "~/models/post.server";
+import { createPost, deletePost, getPost, updatePost } from "~/models/post.server";
 
 export async function loader({ params }: LoaderArgs) {
   invariant(params.slug, "slug not found");
@@ -23,12 +23,17 @@ export async function loader({ params }: LoaderArgs) {
 }
 
 // 🐨 you'll need the `params` in the action
-export async function action({ request }: ActionArgs) {
+export async function action({ request, params }: ActionArgs) {
   const formData = await request.formData();
   // 🐨 grab the "intent" from the form data
+  const intent = formData.get("intent");
 
   // 🐨 if the intent is "delete" then delete the post
   // and redirect to "/posts/admin"
+  if(params.slug !== undefined && intent === "delete") {
+    await deletePost(params.slug);
+    return redirect('/posts/admin');
+  }
 
   const title = formData.get("title");
   const slug = formData.get("slug");
@@ -50,7 +55,17 @@ export async function action({ request }: ActionArgs) {
 
   // 🐨 if the params.slug is "new" then create a new post
   // otherwise update the post.
-  await createPost({ title, slug, markdown });
+  switch (intent) {
+    case "create":
+      await createPost({ title, slug, markdown });
+      break;
+    case "update":
+      await updatePost({title, slug, markdown});
+      break;
+    default:
+      throw new Error("Invalid intent");
+      break;
+  }
 
   return redirect("/posts/admin");
 }
@@ -65,10 +80,14 @@ export default function PostAdmin() {
   // 🐨 now that there can be multiple transitions on this page
   // we'll need to disambiguate between them. You can do that with
   // the "intent" in the form data.
+  const intent = useTransition().submission?.formData.get("intent");
   // 💰 transition.submission?.formData.get("intent")
   const isCreating = Boolean(transition.submission);
   // 🐨 create an isUpdating and isDeleting variable based on the transition
+  const isUpdating = intent === "updating";
+  const isDeleting = intent === "updating";
   // 🐨 create an isNewPost variable based on whether there's a post on `data`.
+  const isNewPost = !data.post;
 
   return (
     <Form method="post">
@@ -127,16 +146,21 @@ export default function PostAdmin() {
       {/* 💰 The button's "name" prop should be "intent" and the "value" prop should be "delete" */}
       {/* 💰 Here's some good looking classes for it: className="rounded bg-red-500 py-2 px-4 text-white hover:bg-red-600 focus:bg-red-400 disabled:bg-red-300" */}
       {/* 🐨 It should say "Deleting..." when a submission with the intent "delete" is ongoing, and "Delete" otherwise. */}
+      {!isNewPost ? <button type="submit" name="intent" value="delete" className="rounded bg-red-500 py-2 px-4 text-white hover:bg-red-600 focus:bg-red-400 disabled:bg-red-300">
+        {isDeleting ? "Deleting..." : "Delete"}
+      </button> : null}
       <p className="text-right">
         <button
           type="submit"
           // 🐨 add a name of "intent" and a value of "create" if this is a new post or "update" if it's an existing post
+          name="intent"
+          value={isCreating ? "create" : "update"}
           className="rounded bg-blue-500 py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400 disabled:bg-blue-300"
           // 🐨 this should be disabled if we're creating *or* updating
-          disabled={isCreating}
+          disabled={isCreating || isUpdating}
         >
           {/* 🐨 if this is a new post then this works fine as-is, but if we're updating it should say "Updating..." / "Update" */}
-          {isCreating ? "Creating..." : "Create Post"}
+          {!isNewPost ? isCreating ? "Creating..." : "Create Post" : isUpdating ? "Updating..." : "Update Post"}
         </button>
       </p>
     </Form>
