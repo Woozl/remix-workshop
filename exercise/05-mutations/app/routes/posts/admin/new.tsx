04eb21c6af6604d49833c7941a3605ef375a3ec4
David Glymph <Woozl@users.noreply.github.com>
2022-10-31 14:43:17 -0400

    05 error handling

--- exercise/05-mutations/app/routes/posts/admin/new.tsx
+++ exercise/05-mutations/app/routes/posts/admin/new.tsx
@@ -5,17 +5,33 @@
 // 4. call the createPost function from your post.model.ts
 // 5. redirect to "/posts/admin".
 
-import { Form } from "@remix-run/react";
-import { ActionArgs } from "@remix-run/node";
+import { Form, useActionData } from "@remix-run/react";
+import { ActionArgs, json } from "@remix-run/node";
 import { redirect } from "@remix-run/node";
 import { createPost } from "../../../models/post.server";
+import invariant from "tiny-invariant";
 
 
 export async function action({request}: ActionArgs) {
   const formData = await request.formData();
-  const title = formData.get('title')!.toString();
-  const slug = formData.get('slug')!.toString();
-  const markdown = formData.get('markdown')!.toString();
+  const title = formData.get('title');
+  invariant(typeof title === "string", "title isn't a string");
+  const slug = formData.get('slug');
+  invariant(typeof slug === "string", "slug isn't a string");
+  const markdown = formData.get('markdown');
+  invariant(typeof markdown === "string", "markdown isn't a string");
+
+  const errors = {
+    title: !title ? "Title required" : "",
+    slug: !slug ? "Slug required" : "",
+    markdown: !markdown ? "Markdown required": "",
+  }
+
+  const hasErrors = Object.values(errors).some((error) => error !== "");
+  if (hasErrors) {
+    return json(errors);
+  }
+  
   const newPost = await createPost(title, slug, markdown);
   return redirect(`/posts/${newPost.slug}`);
 }
@@ -23,6 +39,8 @@ export async function action({request}: ActionArgs) {
 const inputClassName = `w-full rounded border border-gray-500 px-2 py-1 text-lg`;
 
 export default function NewPost() {
+  const errors = useActionData<typeof action>();
+  
   return (
     // 🐨 change this to a <Form /> component from @remix-run/react
     // 🐨 and add method="post" to the form.
@@ -30,17 +48,26 @@ export default function NewPost() {
       <p>
         <label>
           Post Title:{" "}
+          {errors?.title ? (
+            <em className="text-red-600">{errors.title}</em>
+          ) : null}
           <input type="text" name="title" className={inputClassName} />
         </label>
       </p>
       <p>
         <label>
           Post Slug:{" "}
+          {errors?.slug ? (
+            <em className="text-red-600">{errors.slug}</em>
+          ) : null}
           <input type="text" name="slug" className={inputClassName} />
         </label>
       </p>
       <p>
         <label htmlFor="markdown">Markdown: </label>
+        {errors?.markdown ? (
+            <em className="text-red-600">{errors.markdown}</em>
+          ) : null}
         <br />
         <textarea
           id="markdown"
